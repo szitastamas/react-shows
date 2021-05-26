97574b6bc6e4f84097b5958de5e7d01dc7f4400d
szitastamas <tamas.szitas@lorent-online.com>
2021-05-26 10:39:16 +0200

    testing and minor changes

--- src/components/layout/Home.tsx
+++ src/components/layout/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className="home-container">
+        <div id="home">
             <h1>React Shows</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium commodi ex perferendis iure asperiores error dolores dolor impedit harum libero. Neque molestiae aut ratione velit, tempore quam, deleniti iste aspernatur asperiores animi non sint fuga voluptatibus praesentium unde labore dolore consectetur doloribus laboriosam cumque odit sequi. Perspiciatis cupiditate, fugit sequi ex repellendus quisquam corporis veniam reiciendis assumenda dolor, quasi in architecto deleniti culpa quos earum ea numquam corrupti exercitationem dolorem, asperiores nulla.</p>
         </div>
